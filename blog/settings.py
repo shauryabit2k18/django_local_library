8f7fceb7530629c2e3104e1d059f3b40ccf58a7a
Shaurya Sinha <ShauryaSinha@Rakeshs-MacBook-Pro.local>
2020-04-19 10:53:04 +0530

    com

--- blog/settings.py
+++ blog/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'whitenoise.runserver_nostatic',
 
     'allauth',
     'allauth.account',
@@ -140,7 +141,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Tinymce
 
